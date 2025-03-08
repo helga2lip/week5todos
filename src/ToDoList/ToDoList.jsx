a4ced517a94411fae9fad93646c72b07baa5db30
helga2lip <helga2lip@gmail.com>
2025-03-08 11:34:12 +0300

    поиск

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
 import { ToDo } from '../assets/todo/todo'
+import { Button } from '../Button/Button'
 import styles from './ToDoList.module.css'
 
 export function ToDoList(props) {
+  const [inputText, setInputText] = useState('');
+  const [filteredTodos, setFilteredTodos] = useState(props.todos);
 
-  return <ul className={styles.list}>
-    {props.todos.map((todo) => {
-      return <ToDo key={todo.id} todo={todo} onDeleteToDo={props.onDeleteToDo} />
-    })}
+  const onSearchClick = () => {
+    setFilteredTodos(props.todos.filter((todo) => {
+      return todo.title.includes(inputText)
+    }))
+  }
 
-  </ul>
+  const onSearchInputChange = (event) => {
+    setInputText(event.target.value);
+  }
+
+  return <>
+    <div className={styles.searchBlock}>
+      <input className={styles.searchInput} type="text" onChange={onSearchInputChange} value={inputText} />
+      <Button onClick={onSearchClick}>Поиск</Button>
+    </div>
+    <ul className={styles.list}>
+      {filteredTodos.map((todo) => {
+        return <ToDo key={todo.id} todo={todo} onDeleteToDo={props.onDeleteToDo} />
+      })}
+    </ul>
+  </>
 }
